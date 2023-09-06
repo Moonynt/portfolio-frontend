4eef6e2e201d3bf75b8860ee36ac6172c6505fc5
Moonynt <valentinoroman0921@gmail.com>
2023-09-06 00:05:15 -0300

    avancejs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const input = document.querySelector('input');
 const log = document.getElementById('nom');
 const name = document.querySelector('.inputdiv');
 
+const job = document.querySelector('.lab-container');
+const opinion = document.querySelector('.op-container');
+
+let trabajo = false
+let opiniones = false
+
 input.addEventListener('input', updateValue);
 
 function updateValue(e){
@@ -25,3 +31,50 @@ function opacityChange(){
     }
 }
 
+function jobMostrar(){
+    job.style.display = "block";
+    trabajo = true;
+    job.scrollIntoView();
+}
+
+function jobOcultar(){
+    job.style.display = "none";
+    trabajo = false;
+}
+
+function jobOpacity(){
+    if(job.style.display == "block"){
+        jobOcultar();
+    }
+    else if(opiniones == true){
+        opOcultar();
+        jobMostrar();
+    }
+    else{
+        jobMostrar();
+    }
+}
+
+function opMostrar(){
+    opinion.style.display = "block";
+    opiniones = true;
+    opinion.scrollIntoView();
+}
+
+function opOcultar(){
+    opinion.style.display = "none";
+    opiniones = false;
+}
+
+function opiOpacity(){
+    if(opinion.style.display == "block"){
+        opOcultar();
+    }
+    else if(trabajo == true){
+        jobOcultar();
+        opMostrar();
+    }
+    else{
+        opMostrar();
+    }
+}
